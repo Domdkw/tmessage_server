2c581aa48b87b0a5225c10cb2a60d400eedbfc4d
Domdkw <domdkw@qq.com>
2026-06-08 22:11:17 +0800

    Bearer token解析方法|预留ws

--- main.ts
+++ main.ts
@@ -18,7 +18,7 @@ teacher.post('/:teacherId/login', async(c) => {
     const teacherId = c.req.param('teacherId')
     const q = await c.req.json<{username: string, password: string}>()
     if (!q.username || !q.password) {
-        return c.json( Utils.RJson({}, 400, 'Login failed', false) );
+        return c.json( Utils.RJson({}, 400, 'Login failed', false) ,400);
     }
 
     const result = await Login.teacher({
@@ -28,20 +28,20 @@ teacher.post('/:teacherId/login', async(c) => {
     })
     return c.json(result)
     }catch(e){
-        return c.json( Utils.RJson({e:(e as Error).message}, 500, 'try', false) )
+        return c.json( Utils.RJson({e:(e as Error).message}, 500, 'json', false) ,500)
     }
 })
 
 teacher.post('/:teacherId/refresh', async(c) => {
     const teacherId = c.req.param('teacherId')
-    const q = c.req.query('token')
-    if (!q) {
-        return c.json( Utils.RJson({}, 400, 'Refresh failed', false) );
+    const token = Utils.getBearerToken(c)
+    if (!token) {
+        return c.json( Utils.RJson({}, 400, 'Refresh failed', false) ,400);
     }
 
     const result = await Login.refresh({
         userid: teacherId!,
-        token: q || '',
+        token: token,
     })
     return c.json(result)
 })
@@ -49,7 +49,7 @@ teacher.post('/:teacherId/refresh', async(c) => {
 classroom.post('/:classroomId/get_bind_token', async(c) => {
     const classroomId = c.req.param('classroomId')
     if (!classroomId) {
-        return c.json( Utils.RJson({e:'ct1'}, 400, 'Request failed', false) );
+        return c.json( Utils.RJson({e:'ct1'}, 400, 'Request failed', false) ,400);
     }
     const result = await Bind.generateClassroomToken(classroomId!)
     return c.json(result)
@@ -58,38 +58,36 @@ classroom.post('/:classroomId/get_bind_token', async(c) => {
 teacher.post('/:teacherId/bind', async(c) => {
     try{
     const teacherId = c.req.param('teacherId')
-    const q = await c.req.json<{c: string, h: string, t: string}>()
-    if (!q.c || !q.h || !q.t) {
-        return c.json( Utils.RJson({e:'tb1'}, 400, 'Request failed', false) );
+    const q = await c.req.json<{c: string, h: string}>()
+    const token = Utils.getBearerToken(c);
+    if (!q.c || !q.h || !token) {
+        return c.json( Utils.RJson({e:'tb1'}, 400, 'Request failed', false) ,400);
     }
     const result = await Bind.teacherBind({
         teacherId: teacherId!,//写入
-        teacherToken: q.t,//验证
+        teacherToken: token,//验证
         classroomId: q.c,//写入
         classroomToken: q.h,//临时验证
     })
     return c.json(result)
     }catch(e){
-        return c.json( Utils.RJson({e:(e as Error).message}, 500, 'try', false) )
+        return c.json( Utils.RJson({e:(e as Error).message}, 500, 'json', false) ,500)
     }
 })
 
 teacher.post('/:teacherId/unbind', async(c) => {
-    try{
     const teacherId = c.req.param('teacherId')
-    const q = await c.req.json<{c: string, t: string}>()
-    if (!q.c || !q.t) {
-        return c.json( Utils.RJson({e:'tb2'}, 400, 'Request failed', false) );
+    const classroomId = c.req.query('classroomId')
+    const token = Utils.getBearerToken(c);
+    if (!classroomId || !token) {
+        return c.json( Utils.RJson({e:'tb1'}, 400, 'Request failed', false) );
     }
     const result = await Bind.teacherUnbind({
         teacherId: teacherId!,//写入
-        classroomId: q.c,//写入
-        teacherToken: q.t,//验证
+        teacherToken: token,//验证
+        classroomId: classroomId,//写入
     })
     return c.json(result)
-    }catch(e){
-        return c.json( Utils.RJson({e:(e as Error).message}, 500, 'try', false) )
-    }
 })
 
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,35 @@
+import type { StatusCode } from 'hono/utils/http-status'
+import type { Context } from 'hono'
+
+type RJson<T> = {
+    code: StatusCode,
+    data: T,
+    msg: string,
+    success: boolean
+}
 const Utils = {
-    RJson(data: object, code: number, msg: string, success: boolean) {
-        if (!data) {
-            data = {};
-        }
+    RJson<T>(data: T,  code: StatusCode, msg: string, success: boolean): RJson<T> {
         const res = {
-            code: code || 0,
+            code: code,
             data: data,
-            msg: msg || '',
-            success: success || false
+            msg: msg,
+            success: success
         }
         return res;
     },
     generateToken() {
         const token = Array.from({ length: 16 }, () => Math.floor(Math.random() * 16).toString(16)).join('');
+        return token;
+    },
+    getBearerToken(c: Context){
+        const header = c.req.header('Authorization');
+        if (!header) {
+            return null;
+        }
+        const [scheme, token] = header.split(' ');
+        if (scheme.toLowerCase() !== 'bearer') {
+            return null;
+        }
         return token;
     }
 }
